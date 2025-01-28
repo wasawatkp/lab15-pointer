5ca92cdd2cf13d1e5b154fc31aee87d5755e5ea1
wasawatkp <wasawat.upper@gmail.com>
2025-01-28 15:15:48 +0700

    lab15_1 clear

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -1,9 +1,68 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
+    
+    int a = 5;
+    char b = 'A';
+    char &c = b;
+    int *x = &a;
+    char *y = &b;
+    int **z = &x;
+    
+    cout << a << " ";
+    cout << b << " ";
+    cout << c << " ";
+    cout << x << " ";
+    cout << (void * )y << " ";
+    cout << z << " " << "\n";
+    
+    cout << &a << " ";
+    cout << (void * )&b << " ";
+    cout << (void * )&c << " ";
+    cout << &x << " ";
+    cout << &y << " ";
+    cout << &z << " " << "\n";
+    
+    c = 'F';
+    
+    cout << a << " ";
+    cout << b << " ";
+    cout << c << " ";
+    cout << x << " ";
+    cout << (void * )y << " ";
+    cout << z << " " << "\n";
 
+	*y = 'W';
+    
+    cout << a << " ";
+    cout << b << " ";
+    cout << c << " ";
+    cout << x << " ";
+    cout << (void * )y << " ";
+    cout << z << " " << "\n";
+    
+    *x = 6;
+    
+    cout << a << " ";
+    cout << b << " ";
+    cout << c << " ";
+    cout << x << " ";
+    cout << (void * )y << " ";
+    cout << z << " " << "\n";
+    
+    **z = 7;
+    
+    cout << a << " ";
+    cout << b << " ";
+    cout << c << " ";
+    cout << x << " ";
+    cout << (void * )y << " ";
+    cout << z << " " << "\n";
+    
 	
 	
 	return 0;
 }
+	
+	
